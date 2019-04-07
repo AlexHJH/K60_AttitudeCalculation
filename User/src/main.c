88783197ceae7d54c5b1cb26855704dda679afdf
Alex <30765323+AlexHJH@users.noreply.github.com>
2019-04-07 18:36:25 +0800

    Update main.c

--- User/src/main.c
+++ User/src/main.c
@@ -19,7 +19,15 @@ void main()
 {
   IIC_init();
   Common_delay(10);
-  BMX055_init();
+  unsigned char FalseCount = 0;
+  while(false == BMX055_init())
+  {
+    FalseCount++;
+    if(FalseCount > 5)
+    {
+      ////加入失败回显   
+    }
+  }
   PIT_IRQ_init(PIT0, 2, PIT0_IRQHandler);
   NVIC_EnableIRQ(PIT0_IRQn);
   EnableInterrupts;
